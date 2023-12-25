02ca563db41d24db050fe1ee0271741f5954697f
Tripathi-Abhinav <sirabhinavtripathi@gmail.com>
2023-12-25 19:22:42 +0530

    updated with new infos in merge_commands.c

--- merge_commands.c
+++ merge_commands.c
@@ -1,7 +1,9 @@
 /*
 merging code
 
-way 1.  git diff <branch name>  
+way 1.  git diff <branch name>  (to compare commits, files and many more of 
+                                 current branch with the branch whose name 
+                                 is given after 'git diff')
         git merge <branch name>
 
 way 2.  pull request
