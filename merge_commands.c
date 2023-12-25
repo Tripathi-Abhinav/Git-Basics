608251bc46a6ff437d70912ab5f6a69cef5ffa8f
Tripathi-Abhinav <sirabhinavtripathi@gmail.com>
2023-12-25 19:40:39 +0530

    updated merge_commands.c

--- merge_commands.c
+++ merge_commands.c
@@ -9,5 +9,20 @@ way 1.  git diff <branch name>  (to compare commits, files and many more of
                                  name is given after 'git merge')
 
 way 2.  create a pull request
+                        // Pull Request --> it lets you tell others about 
+                                            changes you have pushed to 
+                                            branch in a respository on GitHub
+                                            
+                                            It generate and send request to
+                                            a main branch to merge it in itself
+
+                                            Person supervising main branch first 
+                                            see pull request(mainly changes done)
+                                            and then decides whether to merge
+                                            it or not. They can comment on it like
+                                            it need further improvement and thus 
+                                            can't be accepted or any comment
+
+
 
 */
