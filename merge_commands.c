5626ebf5af0b3d7777aa9e470567a35b002fa129
Tripathi-Abhinav <sirabhinavtripathi@gmail.com>
2023-12-25 19:27:38 +0530

    merge_command.c is updated with new info

--- merge_commands.c
+++ merge_commands.c
@@ -4,8 +4,10 @@ merging code
 way 1.  git diff <branch name>  (to compare commits, files and many more of 
                                  current branch with the branch whose name 
                                  is given after 'git diff')
-        git merge <branch name>
 
-way 2.  pull request
+        git merge <branch name> (to merge current branch with the branch 
+                                 name is given after 'git merge')
+
+way 2.  create a pull request
 
 */
